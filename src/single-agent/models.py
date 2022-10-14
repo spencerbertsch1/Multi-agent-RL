1887d406251f61ad5f5ccc5a8a480ed1ccdf7838
Spencer Bertsch <spencerbertsch99@gmail.com>
2022-10-13 21:05:21 -0400

    skeleton code

---
+++ src/single-agent/models.py
@@ -0,0 +1,46 @@
+"""
+-------------------------------
+| Dartmouth College           |
+| ENGG 199.09 - Game Theory   |
+| Fall 2022                   |
+-------------------------------
+
+Single Agent RL models
+
+This script contains implementations of single-agent reinforcement learning models such 
+as Q-learning and SARSA. 
+
+This script can be run from the command line using: $ models.py
+"""
+
+# imports
+import numpy as np
+
+class QLearning():
+
+    def __init__(self, env: np.array):
+        self.env = env
+
+    def solve(self):
+        pass
+        # TODO 
+
+
+class SARSA():
+
+    def __init__(self, env: np.array):
+        self.env = env
+
+    def solve(self):
+        pass
+        # TODO 
+
+
+def main():
+    pass
+    # TODO
+
+
+if __name__ == "__main__":
+    main()
+    # TODO - write some test code here
