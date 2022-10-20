e4e9b2fedd9d538f7364cee26b8a7e022d0f3371
Spencer Bertsch <spencerbertsch99@gmail.com>
2022-10-20 19:38:46 -0400

    add foundations for sarsa with random action

--- src/single-agent/models.py
+++ src/single-agent/models.py
@@ -12,9 +12,13 @@ as Q-learning and SARSA.
 
 This script can be run from the command line using: $ models.py
 """
+# local imports 
+from envs import StaticEnv
+from routines import Solution
 
 # imports
 import numpy as np
+import time
 
 class QLearning():
 
@@ -28,19 +32,30 @@ class QLearning():
 
 class SARSA():
 
-    def __init__(self, env: np.array):
-        self.env = env
+    def __init__(self, problem_name):
+        self.problem_name = problem_name
 
-    def solve(self):
-        pass
-        # TODO 
+    def sarsa(self):
+
+        # create a demo board for testing 
+        solution = Solution(problem_name=self.problem_name, model_name='SARSA')
+        env = StaticEnv(board=np.array([[0, 0, 0, 100], [0, np.nan, 0, -100], [0, 0, 0, 0]]), start_position=(2,0), \
+                        solution=solution, goal_positions=((0, 3), (1, 3)))
+    
+        # some test code 
+        for i in range(25):
+            env.print_board()
+            env.random_move()
+            if env.solution.solved is True:
+                print(env.solution)
+                break
+            time.sleep(0.2)
 
 
 def main():
-    pass
-    # TODO
 
+    clf = SARSA(problem_name='Static Goal Seek')
+    clf.sarsa()
 
 if __name__ == "__main__":
     main()
-    # TODO - write some test code here
