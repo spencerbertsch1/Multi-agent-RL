bc9cfa7c2c435202a09eaf49c4a3f03f9eea1217
Spencer Bertsch <spencerbertsch99@gmail.com>
2022-11-03 17:57:21 -0400

    add new board

--- src/single-agent/boards.py
+++ src/single-agent/boards.py
@@ -36,3 +36,18 @@ class board2:
     board_y = board.shape[0]
     start_position = (4,0)
     goal_positions = ((0, 3), (1, 3), (2, 3), (3, 3), (4, 3))
+
+
+class board3:
+    board = np.array([[np.nan, 0, 0, 0, 100], 
+                      [0, 0, np.nan, np.nan, np.nan], 
+                      [0, 0, 0, 0, 0],
+                      [np.nan, np.nan, 0, np.nan, 0],
+                      [0, 0, 0, 0, 0],
+                      [0, np.nan, np.nan, 0, np.nan],
+                      [0, 0, 0, 0, -100],])
+    original_board = board.copy()
+    board_x = board.shape[1]
+    board_y = board.shape[0]
+    start_position = (6,0)
+    goal_positions = ((0, 4), (6, 4))
