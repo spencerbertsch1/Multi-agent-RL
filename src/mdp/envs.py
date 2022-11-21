6aac5a99c94347c3558986a257eb99da6799dbb8
Spencer Bertsch <spencerbertsch99@gmail.com>
2022-11-21 14:05:17 -0500

    revert to older environment

--- src/mdp/envs.py
+++ src/mdp/envs.py
@@ -4,12 +4,9 @@
 | ENGG 199.09 - Game Theory   |
 | Fall 2022                   |
 -------------------------------
-
 Single Agent RL models
-
 This script will be used to generate the static or dynamic environments that the 
 RL models will be learning in. To start we will use simple, static environments. 
-
 This script can be run from the command line using: $ envs.py
 """
 
@@ -47,42 +44,18 @@ class MDPStaticEnv():
         self.VERBOSE = VERBOSE
         self.big_board = self.expand_board(board=self.board)
         self.timestamp = timestamp  # measured in base time units defined in the MDP formulation 
-
-
-    def centeroidnp(self):
-        """
-        https://stackoverflow.com/questions/23020659/fastest-way-to-calculate-the-centroid-of-a-set-of-coordinate-tuples-in-python-wi
-        """
-        # get burning node location
-        old_node_locations_arr: tuple = np.where(self.board == 1)
-        # old_node_location_tuple = [[x[0][0], x[1][0]] for x in old_node_location]
-
-        old_node_locations = []
-        for i in range(0, len(old_node_locations_arr), 2):
-            burning_node: tuple = (old_node_locations_arr[i][0], old_node_locations_arr[i+1][0])
-            old_node_locations.append(burning_node)
-
-        arr = np.array(old_node_locations)
-
-        length = arr.shape[0]
-        sum_x = np.sum(arr[:, 0])
-        sum_y = np.sum(arr[:, 1])
-        return sum_x/length, sum_y/length
         
 
     def get_neighbors(self, node_location: tuple) -> tuple:
         """
         Return the neighbors of the agent's location given a 4 neighbor model. 
-
         --- NOTE --- 
         Numpy assumes that the (0, 0) index is in the upper left corner. Use the below matrix as a guide to understand
         how indexing works here: 
-
         [(0,0), (0,1), ..., (0,n)]
         [(1,0), (1,1), ..., (1,n)]
         [ ...    ...         ... ]
         [(m,0), (m,1), ..., (m,n)]
-
         :param: agent_location - 2-length tuple representing the zero indexed location of the agent on the grid
         :return: tuple representing the successors of the agent's current location
         """
@@ -92,32 +65,31 @@ class MDPStaticEnv():
         y = node_location[0]
 
         # get all the neighbors in the 8 neighbor model 
+        # ----- vvv This code snipet was borrowed from a project that was completed last year (it was modified slighly for this project) 
+        s1: list = [(x2, y2) for x2 in range(x-1, x+2)
+                                            for y2 in range(y-1, y+2)
+                                            if (-1 < x < self.board_x and -1 < y < self.board_y and
+                                                (x != x2 or y != y2) and (0 <= x2 < self.board_x) and (0 <= y2 < self.board_y))]
+        # ----- ^^^
 
+        # remove neighbors that arent in the 4 neighbor model (we could actualy remove this code later to use the 8 neighbor model)
         s2 = []
-        s1: list = [(y-1, x), (y, x-1), (y+1, x), (y, x+1)]
-        for location in s1: 
-            x = location[1]
-            y = location[0]
-
-            if ((x < 0) | (x >= self.board_x) | (y < 0) | (y >= self.board_y)):
-                # print(f'Illegal Node: {location}')
-                pass
-            else:
-                # print(f'Legal Node: {location}')
-                s2.append(location)
+        for neighbor in s1:
+            if (neighbor[0] == self.agent_position[1]) | (neighbor[1] == self.agent_position[0]):
+                s2.append(neighbor)
 
         # remove blocking squares (the agent can't move to the squares represented by np.nan)
         successors = []
         for position in s2:
             px = position[1]
             py = position[0]
-            if np.isnan(self.board[py][px]):
+            if np.isnan(self.board[px][py]):
                 pass  # don't append the nans! 
             else:
                 successors.append(position)
 
         # fix a bug in the indexing by flipping the values in the tuples
-        # successors = [(x[1], x[0]) for x in successors]
+        successors = [(x[1], x[0]) for x in successors]
 
         return successors
 
@@ -144,6 +116,7 @@ class MDPStaticEnv():
         """
         simple function to initialize the agent's position and the fire on the board 
         """
+        self.board = self.empty_board.copy()
         # initialize the single agent - later this can be done in a loop for more agents
         self.agent_board[self.agent_position[0], self.agent_position[1]] = 999
         # initialize the fire - later this can be done in a loop for more fire start locations
@@ -151,45 +124,33 @@ class MDPStaticEnv():
 
     def fire_spread(self):
         """
-        Function that is used to spread the fire via either a stochastic or deterministic model 
-
-        Here we model the fire as a radial spread form the ignition point
+        Utility function that is used to spread the fire via either a stochastic or deterministic model 
         """
         if MDP.stochastic_fire_spread: 
             pass  # implement the stochastic fire simulation later
         else:
 
             # get burning node location
-            old_node_locations_arr: tuple = np.where(self.board == 1)
-            # old_node_location_tuple = [[x[0][0], x[1][0]] for x in old_node_location]
-
-            old_node_locations = []
-            for i in range(0, len(old_node_locations_arr), 2):
-                burning_node: tuple = (old_node_locations_arr[i][0], old_node_locations_arr[i+1][0])
-                old_node_locations.append(burning_node)
+            old_node_location = np.where(self.board == 1)
+            old_node_location_tuple = (old_node_location[0][0], old_node_location[1][0])
 
             # new burning node location
-            new_burning_nodes = set()
-            for node_location in old_node_locations:
-                neighbors: tuple = self.get_neighbors(node_location=node_location)
-                for neighbor in neighbors:
-                    if self.board[neighbor[0], neighbor[1]] == 0:
-                        new_burning_nodes.add(neighbor)
-            
-            # new_burning_node_location = tuple([i+1 for i in old_node_location_tuple])
-
-            # update the board to reflect the burn 
-            for old_node_location in old_node_locations:
-                self.board[old_node_location[0], old_node_location[1]] = 2
-
-            for new_burning_node in new_burning_nodes:
-                self.board[new_burning_node[0], new_burning_node[1]] = 1
+            new_burning_node_location = tuple([i+1 for i in old_node_location_tuple])
 
             # now we need to test whether or not the fire has burned out 
-            if len(new_burning_nodes) == 0: 
+            if (new_burning_node_location[0] >= self.board_y) | (new_burning_node_location[1] >= self.board_x): 
                 self.solution.solved = True
                 if self.VERBOSE:
                     print('The fire has gone out! This episode is now complete.')
+
+            elif (self.board[new_burning_node_location[0], new_burning_node_location[1]] == 9): 
+                self.solution.solved = True
+                if self.VERBOSE:
+                    print('The fire has gone out! This episode is now complete.')
+            else:
+                # update the board to reflect the burn 
+                self.board[new_burning_node_location[0], new_burning_node_location[1]] = 1
+                self.board[old_node_location[0], old_node_location[1]] = 2
 
     def print_board(self):
         """
@@ -253,7 +214,6 @@ class MDPStaticEnv():
     def increment_time(self, action: int):
         """
         This method increments the environment forward by one base time unit. 
-
         If the agent moves every 2 time units and the fire moves every 10 time units, this is the method
         that keeps track of this information, logging the time and moving the state of the system forward. 
         """
@@ -269,7 +229,7 @@ class MDPStaticEnv():
             # here we initiate the stoachastic or deterministic action for the fire
             self.fire_spread()
 
-    def calculate_final_reward(self, action: int):
+    def calculate_final_reward(self):
         """
         Small method that returns the reward given the current state
         """
@@ -281,44 +241,18 @@ class MDPStaticEnv():
         Small method that returns the reward given the current state, action pair
         """
         # reward for dropping phos chek in fire's path
-        # if ((action == 4) & (self.agent_position[0] == 2) & (self.agent_position[1] < 7)):
-        #     # ^^ this second check is only useful because of the linear fire spread - remove for radial fire spread
-        #     # here we calculate the reward based on the linear distance from the fire start location
-        #     R = 20
-        #     # here we center the linear surface on the position (1,1), so the maximal reward is achieved by dropping 
-        #     # phos chek directly in front of the fire. Rewards for dropping phos check spread equally from this point. 
-        #     abs_diff = abs(sum(self.agent_position) - 2)
-        #     reward = R - abs_diff
-        #     return reward
-        # else:
-        #     return 0
-
-        R = 0
-        if (action == 3) & (self.agent_position[0] == 2):
-            R = 100
-        # if ((action == 3) & (self.agent_position[1] >= 7) & (self.agent_position[0] == 2)):
-        #     # ^^ this second check is only useful because of the linear fire spread - remove for radial fire spread
-        #     # here we calculate the reward based on the linear distance from the fire start location
-        #     R = 100
-        # elif ((action == 3) & (self.agent_position[1] < 7) & (self.agent_position[0] == 2)):
-        #     # here we center the linear surface on the position (1,1), so the maximal reward is achieved by dropping 
-        #     # phos chek directly in front of the fire. Rewards for dropping phos check spread equally from this point. 
-        #     R = 50
-        # elif ((action == 4) & (self.agent_position[1] < 7)):
-        #     R = 200
-        # elif (action == 1):
-        #     R = -100
-
-        return R
-
-
-        # # fire_centroid = self.centeroidnp()
-        # fire_centroid = [2, 6]
-        # if ((fire_centroid[0] == self.agent_position[0]) & (fire_centroid[1] == self.agent_position[1])):
-        #     return 200
-        # else:
-        #     reward = 1/(math.dist(fire_centroid, self.agent_position))
-        #     return reward*100
+        if ((action == 4) & (self.agent_position[0] == self.agent_position[1]) & (self.agent_position[0] <= 1)):
+            # ^^ this second check is only useful because of the linear fire spread - remove for radial fire spread
+            # here we calculate the reward based on the linear distance from the fire start location
+            R = 10
+            # here we center the linear surface on the position (1,1), so the maximal reward is achieved by dropping 
+            # phos chek directly in front of the fire. Rewards for dropping phos check spread equally from this point. 
+            abs_diff = abs(sum(self.agent_position) - 2)
+            reward = R - abs_diff
+            # print(reward)
+            return reward
+        else:
+            return 0
 
 def make_movie():
     """
